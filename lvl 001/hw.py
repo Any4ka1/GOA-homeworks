34cae7fa19f2c0e7c67ba3587c169877d4d2f588
any4ka <garaevianano@gmail.com>
2025-07-07 18:39:52 +0400

    Add files via upload

---
+++ lvl 001/hw.py
@@ -0,0 +1,21 @@
+print ("my favourite colour is dark purple")
+print ("my favourite food is sushi")
+print ("my favourite hobby is drawing and listening to music") 
+
+#variable and value about my favourite movies
+movie1 = "Insidious: The Red Door"
+movie2 = "squid game"
+movie3 = "final destination 6"
+movie4 = "harry potter"
+movie5 = "atak on titan" #anime because i dont watch movies
+
+#What does concatination mean? concatination is when we add two or more strings to each other
+
+
+#Data type. We have 2 types of datas: text data, numeric data.
+#Text data - string -- "cat"
+#Numeric data - 1, 2, 3, 4, -- (integer) /  1.2 , 1.3 , 1.4 , 1.5 , -- (float) /   5 + 9j -- (imaganary)
+
+name = "anano"
+age = 14
+print ("hello my name is " + name + " and I am " + str(age) + " years old")
