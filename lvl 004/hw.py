7d1fee7bdce60a2d49570e9adf361acc72b304ab
any4ka <garaevianano@gmail.com>
2025-07-15 23:08:45 +0400

    hw

---
+++ lvl 004/hw.py
@@ -0,0 +1,37 @@
+# + & - are just addition and subtraction
+# / is division // is integer division
+# * is multiplication ** is exponentiation % is modulus
+
+number_1 = 2
+number_2 = 3
+
+number = input("enter your number: ")
+
+print(str(number_1) + "+" + str(number_2) + "=" + number)
+
+
+plus = 10 + 4 + 4
+minus = 10 - 4 - 4
+divide_1 = 10 // 4 // 4 
+divide_2 = 10 / 4 / 4
+mutify = 5 * 5
+axarisxeba = 6 ** 2
+nashti = 48 % 5
+
+
+#amocanis amoxsna
+yuti_1 = 4          #x= 4 * 3
+yuti_3 = "x"        #x= 12
+
+print(4 * 3)
+
+
+number_1 = int(input("enter your age: "))
+number_2 = int(input("enter yout age: "))
+
+print(number_1 > number_2)
+print(number_1 < number_2)
+print(number_1 >= number_2)
+print(number_1 <= number_2)
+print(number_1 == number_2)
+print(number_1 != number_2) 
