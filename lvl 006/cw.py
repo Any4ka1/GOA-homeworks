6e13b04d143b06f58b505d1cdc9a196e74c18111
any4ka <garaevianano@gmail.com>
2025-07-22 15:43:34 +0400

    .

---
+++ lvl 006/cw.py
@@ -0,0 +1,27 @@
+#str
+str1= "green"
+str2= "ogre"
+result = str1 + " " + str2
+print(result)
+
+#int
+age = 14
+year = 2025
+temperature = +420
+
+#float
+price = 69.69
+pi = 3.14
+temperature = 37.6
+
+#bool
+is_active = True
+has_permission = False
+is_raining = True
+
+#concatenation
+print('snow' + 'is' + 'very' + 'good')
+
+
+num = int("5")  
+text = str(5)
---
+++ lvl 006/hw.py
@@ -0,0 +1,15 @@
+# boolean is True if the given number is even, False otherwise (< > <= >= == =!)
+
+
+# str. -- "toy", 'cup' , 'hand' , 'blanket' , 'candle' 
+#int. -- 2 , 3 , 4 , 5 , 8 , (numbers)
+#float. -- 1,2 ; 4,3 ; 5,6 ; 8,7 ; 9,4 (not full numbers)
+#boolean -- < > <= >= == =! 
+
+number_1 = int(input("enter your number: "))
+number_2 = int(input("enter your number: "))
+
+print(number_1 > number_2)
+print(number_1 < number_2)
+print(number_1 <= number_2)
+print(number_1 >= number_2)
