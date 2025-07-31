ec7621cc2dc5fac39623b99dc9b3168ef0879940
any4ka <garaevianano@gmail.com>
2025-07-31 23:15:04 +0400

    hw

---
+++ lvl009/hw.py
@@ -0,0 +1,60 @@
+number = int(input("enter your number: "))
+if number > 18:
+    print("xan ride a car!")
+
+else:
+    print("can`t ride a car")
+
+
+
+
+if 20 < 35:
+    print("outside is warm, you sould go out.")
+
+elif 0 < 20:
+    print("its cold stay at home.")
+else:
+    print("stay at home")
+
+
+
+value_1 = 0
+value_2 = 5
+
+if value_1 < value_2:
+    print("buy it")
+
+elif 0 > 5:
+    print("buy cheese")
+
+elif 5 > 0:
+    print("buy bread")
+
+else:
+    print("buy milk")
+
+
+
+
+
+value_1 = int(input("your value: "))
+if value_1 >= 50:
+    print("you will receive this product")
+else:
+    print("you will not receive this product")
+
+
+
+number = int(input("your number: "))
+if number % 2 == 1:
+    print("number is odd")
+    if number >= 1 and number <= 9:
+        print("this is a single digit odd number")
+    elif number % 9 >= 1:
+        print("this is a multi-digit odd number")
+    else:
+        print("this number is even") 
+
+
+
+        
