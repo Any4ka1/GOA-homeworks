e697b92e0e39f142a8b9ee48384e2b7954430960
any4ka <garaevianano@gmail.com>
2025-08-08 19:35:05 +0400

    hw

---
+++ lvl011/cw.py
@@ -0,0 +1,5 @@
+for _ in range(1000):
+    print("GOA მაგარია!")
+
+for i in range(1000):
+    print("GOA მაგარია!" + str(i))
---
+++ lvl011/hw.py
@@ -0,0 +1,18 @@
+#ინდენტაცია for loopis dros aucilebelia radgan mis gareshe amogvigdebs errors.
+
+
+for i in range (7):
+    print(4)
+
+for i in range (8):
+    print("I love cats")
+
+name = input("enter your name: ")
+for i in range (5):
+    print("hello " + name)
+
+#for i in range5
+#print("i")
+
+for i in range (5):
+    print("i")
