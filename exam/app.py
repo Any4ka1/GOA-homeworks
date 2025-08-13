212566d1243f99463d18dfe2fb9c19cb101a0444
any4ka <garaevianano@gmail.com>
2025-08-13 15:32:30 +0400

    hw

---
+++ exam/app.py
@@ -0,0 +1,44 @@
+number = int (input("Enter a number: "))
+
+result = number * 5
+
+print (result)
+
+number = int(input("enter ur number:"))
+
+if number >10:
+    print("more than 10")
+
+else: 
+    print("less than 10")
+
+
+for i in range(1, 10):
+
+ print ("anano")
+
+ # ახსენი რას აკეთებს if, elif,else
+ # if cheks the first condition, elif 
+#მომხმარებელს შემოატანინე თავის სახელი და გამოპრინტე "გამარჯობა {"მომხმარებლის სახელი"}
+
+name = (input("enter ur name:"))
+print ("Hello" +name)
+#მომხმარებელს შემოატანინე რიცხვი და შეამოწმე არის თუ არა ეს რიცხვი negative,positive თუ 0
+number2=1
+if number2 >0:
+   print ("positive")
+elif number2 <0:
+   print("negative")
+else:
+     print("is equal to 0")  
+
+#მომხმარებელს შემოატანინე enter minutes და ეს წუთები გარდაქმენი წამებში/seconds
+
+time= int (input("enter ur minutes:"))
+result = time * 60
+print(result)
+
+#for loop დახმარებით 5 იდან 20 ამდე გამოიტანე რიცხვები
+
+for anano in range(5, 20):
+   print(anano)
