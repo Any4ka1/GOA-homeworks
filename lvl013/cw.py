e029e6462295215f00ae9254c369ad50a1cffd09
any4ka <garaevianano@gmail.com>
2025-08-15 20:53:36 +0400

    рц

---
+++ lvl013/cw.py
@@ -0,0 +1,5 @@
+for num in range(0, 50):  
+    if num % 2 == 0:      
+        print(num, "is even")
+    else:                 
+        print(num, "is odd")
---
+++ lvl013/hw.py
@@ -0,0 +1,66 @@
+#for loop is used to iterate over a sequence
+#1st task
+number = float(input("Enter a number: "))
+if number >0:
+    print(number,"is positive") 
+elif number < 0:
+    print(number,"is negative")
+else:    
+    print("number is zero")
+#2nd task
+num = int(input("ur number : "))
+
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")    
+#3rd task
+score = int(input("put youre score: "))
+
+if score == 100:
+    print("good job!")
+elif score < 50:
+    print("HAHA loser")
+else:
+    print("u passed")
+#4th task  
+for number in range (10):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
+
+
+
+for number in range (50):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
+
+
+for number in range (5):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
+
+
+
+for number in range (30):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")
+
+
+for number in range (15):
+
+  if number % 2 == 0:
+    print(number ,"luwi")
+  else:
+    print(number ,"kenti")  
