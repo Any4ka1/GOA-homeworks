705545d19f7729073eb5298b06b53b5cd15ddcb6
any4ka <garaevianano@gmail.com>
2025-08-22 20:43:16 +0400

    ほめを

---
+++ lvl015/cw.py
@@ -0,0 +1,7 @@
+for i in range(10,50, 5):
+    print(i)
+
+count = 0
+while count < 50:
+    print("anano ")
+    count += 1
---
+++ lvl015/hw.py
@@ -0,0 +1,17 @@
+#While loopi grdzeldeba sadamde sanam magaliti sworia
+
+#2 davaleba
+i = 4
+while i < 84:
+    print(i)
+    i = i + 6
+
+#3 davaleba
+num = ["kasper", "dzagli", 13, "dog", "cat", 6.7]
+print(num[1])
+print(num[3])
+print(num[5])
+
+
+#4 davaleba
+#listshi shegvidzlia shevinaxot bevri mnishvneloba, magram cvladshi marto erti.
