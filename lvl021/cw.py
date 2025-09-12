fcc95e54e725f2f74c3eabe948554bedbad34878
any4ka <garaevianano@gmail.com>
2025-09-12 20:18:13 +0400

    hw

---
+++ lvl021/cw.py
@@ -0,0 +1,6 @@
+list = [1, 2, 3, 4, 5, 6, 5, 7, 10, 15]
+print(list)
+size = len(list)
+
+for i in range(size):
+    print(list[i])
---
+++ lvl021/cw2.py
@@ -0,0 +1,3 @@
+arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+reversed_array = arry[::-1]
+print(reversed_array)
