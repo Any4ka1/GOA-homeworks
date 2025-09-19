ab36022a42476211e2ff17f615be300f9f526a0f
any4ka <garaevianano@gmail.com>
2025-09-19 17:40:00 +0400

    hw

---
+++ lvl023/cw.py
@@ -0,0 +1,4 @@
+a = "HALLO! IM EMU OTORI! EMU IS MEANING SMILE!"
+
+for i in range(len(a)):
+    print(a[i], ":p")
---
+++ lvl023/cw2.py
@@ -0,0 +1,8 @@
+a = "lom ki ara lekvi kide!"
+print(len(a))
+new_g = ""
+
+for i in range(len(a)):
+    if a[i] != "":
+        new_g += a[i]
+print(new_g)
