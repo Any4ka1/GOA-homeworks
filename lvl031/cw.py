747cac42065e2275e596c1307bb9a7cc4da2ed07
any4ka <garaevianano@gmail.com>
2025-10-17 20:33:46 +0400

    hw

---
+++ lvl031/cw.py
@@ -0,0 +1,7 @@
+def skeleton(b, n):
+    if b + n == 55:
+        return "Here u go 55 bone"
+    else:
+        return "U never get bones!"
+
+print(skeleton(25, 30)) 
---
+++ lvl031/hw.py
@@ -0,0 +1,53 @@
+num = input("enter your number: ")
+def function(num):
+    if num % 2 == 0:
+        return("odd")
+    else:
+        return("even")
+
+bl = input("enter your number: ")
+def uaryofitdadebiit(bl):
+    if bl > 0:
+        return "positive"
+    else:
+        return "negative"
+    
+
+num1 = input("enter your number: ")
+num2 = input("enter your second number: ")
+
+def function(num1, num2):
+    if num1 > num2:
+        return "num1 metia num12-ze"
+    elif num1 < num2:
+        return "num1 naklebia num12-ze"
+    else:
+        return "whut did u write???"
+    
+grade = input("enter your grade: ")
+
+def function():
+    if grade > 90:
+        return "A"
+    elif 89 > grade > 80:
+        return "B"
+    elif 79 > grade > 70:
+        return ("C")
+    elif 69 > grade > 60:
+        return "D"
+    elif 59 > grade > 0:
+        return "F"
+    else:
+        return "w speed"
+    
+weather = input("enter your weather: ")
+
+def function():
+    if 0 > weather:
+        return "Today is very cold! Wear warm clothes 💙"
+    elif weather < 30:
+        return "Today is a really nice weather 🥰"
+    elif weather > 30:
+        return "Today is very hot! Drink plenty of tea🔥"
+    else:
+        return "ur bout to burn"
